b4bd913623d1bb44aa3734a8aa2dad1b186c92ea
lee22417 <jlee22417@gmail.com>
2024-06-23 19:44:43 +0900

    set graphql, typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+// import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ApolloDriver } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        // console.log('DB_PASSWORD:', configService.get<string>('DB_HOST'));
+        return {
+          type: 'mysql',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
+          synchronize: false,
+        };
+      },
+    }),
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema/schema.gql'),
+    }),
+  ],
+  controllers: [],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
